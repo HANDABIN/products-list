5983980f38e4ce9a7f943471d64b31456b7be7cd
HANDABIN <105402278+HANDABIN@users.noreply.github.com>
2022-06-19 21:58:30 +0900

    Add files via upload

---
+++ db.js
@@ -0,0 +1,18 @@
+// 상품 db
+const items = [
+    {
+      imgUrl: 'https://placeimg.com/200/250/animals/1',
+      title: '상품 1',
+      price: '100,000원'
+    },
+    {
+      imgUrl: 'https://placeimg.com/200/250/animals/2',
+      title: '상품 2',
+      price: '50,000원'
+    },
+    {
+      imgUrl: 'https://placeimg.com/200/250/animals/3',
+      title: '상품 3',
+      price: '150,000원'
+    },
+  ];
